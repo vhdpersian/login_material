2ae456bcacd2e81a6bb3641f2a0ed6bb5f86e190
vhdpersian <karimi_vhd@yahoo.com>
2016-07-01 01:09:50 -0700

    2016.07.01

--- app/src/main/java/com/example/administrator/login1/DatabaseHelper.java
+++ app/src/main/java/com/example/administrator/login1/DatabaseHelper.java
@@ -49,7 +49,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     private SQLiteDatabase database;
     public DatabaseHelper( final Context context) {
-        super(new DatabaseContext(context),DATABASE_NAME,null, DATABASE_VERSION);
+        super(context,DATABASE_NAME,null, DATABASE_VERSION);
 
         //  String ROOT_DIR = context.getA getAbsolutePath();
         //String s = context.getDatabasePath(DATABASE_NAME).getPath();
@@ -159,55 +159,3 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
 
 
-
-class DatabaseContext extends ContextWrapper {
-
-    private static final String DEBUG_CONTEXT = "DatabaseContext";
-
-    public DatabaseContext(Context base) {
-        super(base);
-
-        openOrCreateDatabase("test",base.MODE_PRIVATE ,null);
-    }
-
-    @Override
-    public File getDatabasePath(String name)
-    {
-        File sdcard = Environment.getExternalStorageDirectory();
-        String dbfile = sdcard.getAbsolutePath() + File.separator+ "databases" + File.separator + name;
-
-        if (!dbfile.endsWith(".db"))
-        {
-            dbfile += ".db" ;
-        }
-
-        File result = new File(dbfile);
-
-        if (!result.getParentFile().exists())
-        {
-            result.getParentFile().mkdirs();
-        }
-
-        if (Log.isLoggable(DEBUG_CONTEXT, Log.WARN))
-        {
-            Log.w(DEBUG_CONTEXT,
-                    "getDatabasePath(" + name + ") = " + result.getAbsolutePath());
-        }
-
-        return result;
-    }
-
-    @Override
-    public SQLiteDatabase openOrCreateDatabase(String name, int mode, SQLiteDatabase.CursorFactory factory)
-    {
-        SQLiteDatabase result = SQLiteDatabase.openOrCreateDatabase(getDatabasePath(name), null);
-         // SQLiteDatabase result = super.openOrCreateDatabase(name, mode, factory);
-        if (Log.isLoggable(DEBUG_CONTEXT, Log.WARN))
-        {
-            Log.w(DEBUG_CONTEXT,
-                    "openOrCreateDatabase(" + name + ",,) = " + result.getPath());
-        }
-        return result;
-    }
-
-}
