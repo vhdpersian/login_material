8f65fbd3051afd36ed99650fa0ac97d045cd3576
vhdpersian <V_Karimi@iranet.net>
2016-07-02 18:20:30 +0430

    2016.07.02

--- app/src/main/java/com/example/administrator/login1/MusicActivity.java
+++ app/src/main/java/com/example/administrator/login1/MusicActivity.java
@@ -30,7 +30,7 @@ public class MusicActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_music);
        // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
        // setSupportActionBar(toolbar);
 
